96db5888db08fa021311f133af198283c3a08391
WebWAGNER67 <ericwagner.pro@gmail.com>
2024-08-03 11:47:34 +0200

    commands with discord user infos

--- main.py
+++ main.py
@@ -5,6 +5,20 @@ from discord.ext import commands
 
 logger = settings.logging.getLogger("bot")
 
+class Slapper(commands.Converter):
+    use_nicknames : bool
+    
+    def __init__(self, use_nicknames) -> None:
+        self.use_nicknames = use_nicknames
+    
+    
+    async def convert(self, ctx, argument) :
+        someone = random.choice(ctx.guild.members)
+        nickname = ctx.author
+        if self.use_nicknames:
+            nickname = ctx.author.global_name
+        return f"{nickname} slaps {someone} with {argument}"
+
 def run():
     intents = discord.Intents.default()
     intents.message_content=True
@@ -46,6 +60,14 @@ def run():
     @bot.command()
     async def add(ctx, one : int, two : int):
         await ctx.send(one + two)
+        
+    @bot.command()
+    async def joined(ctx, who : discord.Member):
+        await ctx.send(who.joined_at)
+        
+    @bot.command()
+    async def slap(ctx, reason : Slapper(use_nicknames=True)): # type: ignore
+        await ctx.send(reason)
     
     bot.run(settings.DISCORD_API_SECRET, root_logger=True)
     
