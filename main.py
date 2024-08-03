cd89b6d6eb285bde74f11fbb529f40e21eacee04
WebWAGNER67 <ericwagner.pro@gmail.com>
2024-08-03 11:54:31 +0200

    handled error messages

--- main.py
+++ main.py
@@ -29,6 +29,11 @@ def run():
     async def on_ready():
         logger.info(f"User: {bot.user} (ID: {bot.user.id})")
         
+    @bot.event
+    async def on_command_error(ctx, error):
+        if isinstance(error, commands.MissingRequiredArgument):
+            await ctx.send("handled error globally")
+        
     @bot.command(
         aliases=['p'],
         help="This is help",
@@ -68,6 +73,11 @@ def run():
     @bot.command()
     async def slap(ctx, reason : Slapper(use_nicknames=True)): # type: ignore
         await ctx.send(reason)
+    
+    @add.error
+    async def add_error(ctx, error) :
+        if isinstance(error, commands.MissingRequiredArgument) :
+            await ctx.send("handled error locally")
     
     bot.run(settings.DISCORD_API_SECRET, root_logger=True)
     
